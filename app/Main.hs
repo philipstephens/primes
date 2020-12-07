b169c4666129a0436e3bb554e1384982564233df
philipstephens <plusgforce@gmail.com>
2020-12-06 22:46:42 -0600

    Add files via upload

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,13 @@
 module Main where
 
-import Lib
+import GenPrimes
 
 main :: IO ()
-main = someFunc
+main = do
+  putStrLn "Prime Numbers:\n"
+  print $ combPrimes (cleanFilter3 (genRawFilter3 250))
+                     (cleanFilter1 (genRawFilter1 250))
+
+  putStr "\n\nIs 4387 a prime number ? "
+  if (isPrime 4387) == True then print "Yes" else print "No"
+
